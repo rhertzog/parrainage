326c8827d3eba3edbfd4f5be229acbcd8f8b26c9
Raphaël Hertzog <hertzog@debian.org>
2017-02-09 00:08:46 +0100

    Add basic authentication form

--- parrainage/project/settings.py
+++ parrainage/project/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'parrainage.project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,3 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
